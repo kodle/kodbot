88cd42c353741e3e7df56d6406dfe305b89adcd9
kodle <theobourdier1@gmail.com>
2020-07-09 23:59:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const randomnum = require("unique-random");
 const rand = randomnum(1, 6);
 const { MessageEmbed } = require("discord.js");
 const superagent = require("superagent");
-const moment = require("moment");
-moment.locale();
-const time = moment().format("Do MMMM YYYY, h:mm:ss");
 const token = process.env.TOKEN;
 
 const prefix = ",";
@@ -34,29 +31,14 @@ client.on("message", message => {
   const args = message.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
 
-  var member = message.mentions.members.first();
-  const log = "[" + time + "] Commande executé par " + message.author + ": ";
   var snekfetch = require("snekfetch");
 
   // -------- COMMANDES -------
-
-  if (message.content.startsWith(prefix + "rules")) {
-    const embed = new Discord.MessageEmbed()
-      .setColor(0x1abc9c)
-      .setTitle("Bienvenue !")
-      .addField(
-        "Les règles",
-        "Pas de provocations, d'insultes, de violences verbales et toutes autres réactions inutiles.\nPas de flood ni de flame, réglez vos problèmes en privé.\nPas de propos ou images pornographiques, racistes, xénophobes, subversifs, obscènes, homophobe etc...\nPas de liens illégaux dans les discussions, par message ou autre.\nCe discord n'est pas un panneau de pub, donc interdiction de puber son propre Discord personnel ici.\n Tout pseudo, tag ou propos à caractère diffamatoire, antisémite, homophobe, agressif ou injurieux est interdit.\nRespectez-vous les uns les autres et restez courtois, vous êtes tous ici avec un point commun alors pas besoin de faire la bagarre."
-      );
-    message.channel.send(embed);
-    console.log(log + "rules");
-  }
 
   if (message.content.startsWith(prefix + "help")) {
     const embed = new Discord.MessageEmbed()
       .setColor(0x1abc9c)
       .setTitle("Listes des commandes")
-      .addField(",rules", "Envoie les règles")
       .addField(",cat", "Envoie une image aléatoire de chat")
       .addField(",dice", "Fait rouler un dé et donne un nombre entre 1 et 6")
       .addField(",lenny", "( ͡° ͜ʖ ͡°)")
@@ -64,25 +46,9 @@ client.on("message", message => {
     console.log(log + "help");
   }
 
-  if (message.content.startsWith(prefix + "info")) {
-    const embed = new Discord.MessageEmbed()
-      .setColor(0x1abc9c)
-      .setTitle("À propos")
-      .addField("Dev", "@kodle#1857")
-      .addField("GitHub", "https://github.com/kodle/kodbot");
-    message.channel.send(embed);
-    console.log(log + "info");
-  }
-
   if (message.content.startsWith(prefix + "lenny")) {
     message.channel.send("( ͡° ͜ʖ ͡°)");
     console.log(log + "( ͡° ͜ʖ ͡°)");
-  }
-
-  if (message.content.startsWith(prefix + "avatar")) {
-    if (!message.mentions.users.size) {
-      return message.reply(`${message.author.displayAvatarURL}`);
-    }
   }
 
   if (message.content.startsWith(prefix + "cat")) {
@@ -109,16 +75,6 @@ client.on("message", message => {
       .then(message => message.edit(`Le numéro du dé est ${rand()}`));
     console.log(log + `dice ${rand()}`);
   }
-
-client.on("guildMemberAdd", member => {
-  member.guild.channels
-    .get("329322280201224203")
-    .send("Bienvenue sur la Compagnie !");
-});
-
-client.on("guildMemberRemove", member => {
-  if (newUsers.has(member.id)) newUsers.delete(member.id);
-});
 
 module.exports.help = {
   name: "dice"
