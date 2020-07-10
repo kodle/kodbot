62992da37429348256ffbc20433955d7f5f12804
kodle <theobourdier1@gmail.com>
2020-07-10 10:15:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
-const Discord = require("discord.js");
-const newUsers = new Discord.Collection();
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const { get } = require("snekfetch");
-const ms = require("ms");
 const randomnum = require("unique-random");
 const rand = randomnum(1, 6);
+
 const { MessageEmbed } = require("discord.js");
-const superagent = require("superagent");
+const { get } = require("snekfetch");
+
+const prefix = ","
 const token = process.env.TOKEN;
 
-const prefix = ",";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Node version: " + process.version);
+  console.log("DiscordJS version: " + Discord.version);
 
-client.on("ready", () => {
-  client.user.setPresence({ game: { name: 'aaaaaaaaaaa', type: 0 } });
-
-
-  console.log(`===========================`);
-  console.log(`Connecté en tant que ${client.user.tag}`);
-  console.log("Votre token: " + token);
-  console.log("");
-  console.log("Version de Node: " + process.version);
-  console.log("Version de DiscordJS: " + Discord.version);
-  console.log(`===========================`);
+  client.user.setPresence({ game: { name: 'pièce', type: 0 } });
 });
 
-client.on("message", message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).split(" ");
-  const command = args.shift().toLowerCase();
-
-  var snekfetch = require("snekfetch");
-
-  // -------- COMMANDES -------
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong!');
+  }
 
   if (message.content.startsWith(prefix + "help")) {
     const embed = new Discord.MessageEmbed()
       .setColor(0x1abc9c)
       .setTitle("Listes des commandes")
       .addField(",cat", "Envoie une image aléatoire de chat")
-      .addField(",dice", "Fait rouler un dé et donne un nombre entre 1 et 6")
+      .addField(",dice", "Fait rouler un dé entre 1 et 6")
       .addField(",lenny", "( ͡° ͜ʖ ͡°)")
     message.channel.send(embed);
     console.log("help");
-  }
-
-  if (message.content.startsWith(prefix + "lenny")) {
-    message.channel.send("( ͡° ͜ʖ ͡°)");
-    console.log("( ͡° ͜ʖ ͡°)");
   }
 
   if (message.content.startsWith(prefix + "cat")) {
@@ -76,9 +58,15 @@ client.on("message", message => {
     console.log(`dice ${rand()}`);
   }
 
-  module.exports.help = {
-    name: "dice"
-  };
+  if (message.content.startsWith(prefix + "lenny")) {
+    message.channel.send("( ͡° ͜ʖ ͡°)");
+    console.log("lenny");
+  }
+
 });
+
+module.exports.help = {
+  name: "dice"
+};
 
 client.login(token);
