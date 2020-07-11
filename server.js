9c6604ffb4b77808956898dccc8fc7c4ae2ad65c
kodle <theobourdier1@gmail.com>
2020-07-11 11:30:58 +0200

    let member

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(' ');
   const command = args.shift().toLowerCase();
 
-  let member = message.guild.member(message.author);
+  let member = message.mentions.members.first();
+
   var csgo = member.guild.roles.cache.find(role => role.name === "Counter-Strike");
   var mc = member.guild.roles.cache.find(role => role.name === "Minecraft");
   var terraria = member.guild.roles.cache.find(role => role.name === "Terraria");
@@ -71,7 +72,7 @@ client.on('message', message => {
   // -- Commande de rôles --
 
   if (message.content.startsWith(prefix + "csgo")) {
-    message.author.roles.add(csgo).catch(console.error);
+    member.addRole(csgo).catch(console.error);
     message.channel.send("Le rôle a été ajouté.")
   }
 
