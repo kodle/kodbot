86bf8afacec8c5c5e405c30b795076874398068e
kodle <theobourdier1@gmail.com>
2020-07-11 10:53:36 +0200

    update role command

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(' ');
   const command = args.shift().toLowerCase();
 
-  var member = message.author;
   var csgo = member.guild.roles.cache.find(role => role.name === "Counter-Strike");
   var mc = member.guild.roles.cache.find(role => role.name === "Minecraft");
   var terraria = member.guild.roles.cache.find(role => role.name === "Terraria");
@@ -71,8 +70,10 @@ client.on('message', message => {
   // -- Commande de rôles --
 
   if (message.content.startsWith(prefix + "csgo")) {
-    message.reply("le rôle a été ajouté.")
-    member.roles.add(csgo);
+    message.author.addRole(csgo).catch(console.error);
+    message.channel.send("Le rôle a été ajouté.")
+  }
+
   }
 
   // -- GIF --
