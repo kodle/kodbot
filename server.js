c04e0318f13f363e469e83cf8b5399875eaf1a59
kodle <theobourdier1@gmail.com>
2020-07-12 10:26:46 +0200

    fix

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ client.on('message', message => {
   // -- Commande de rôles --
 
   if (message.content.startsWith(prefix + "csgo")) {
-    guildMember.addRole(role);
+    message.member.addRole(role);
     message.reply("le rôle a été ajouté.")
   } else {
       /* if(message.member.role.cache.some(r=>["Counter-Strike"].includes(r.name)) ) {
