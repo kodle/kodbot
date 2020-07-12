6f61c810f92751efb946d472155f5180bb32fb5c
kodle <theobourdier1@gmail.com>
2020-07-12 11:16:38 +0200

    test role

--- server.js
+++ server.js
@@ -75,12 +75,12 @@ client.on('message', message => {
   // -- Commande de rôles --
 
   if (message.content.startsWith(prefix + "csgo")) {
-    guildMember.roles.add(csgo);
-    message.reply("le rôle a été ajouté.")
-  } else {
-      if (message.member.roles.cache.has('650815370256449536')) {
-        message.reply("tu as déjà ce rôle.")
+    if (message.member.roles.cache.has('650815370256449536')) {
+      message.reply("tu as déjà ce rôle.")
     }
+  } else {
+      guildMember.roles.add(csgo);
+      message.reply("le rôle a été ajouté.")
   }
 
   // -- GIF --
