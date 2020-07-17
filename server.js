091044d1fd8826d4e40a4983712099fd4c530b14
kodle <theobourdier1@gmail.com>
2020-07-17 10:52:50 +0200

    guildMember to member

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ const delprefix = prefix + "del";
 const token = process.env.TOKEN;
 // var giphy = require('giphy-api')(process.env.GIPHY);
 
-client.on('guildMemberAdd', (guildMember) => {
-  guildMember.guild.channels.cache.get('589895016231600158').send(guildMember.user.username + ", bienvenue sur la Compagnie !");
-  guildMember.roles.add(guildMember.guild.roles.cache.find(r => r.name === "Membre"));
+client.on('guildMemberAdd', member => {
+  member.guild.channels.cache.get('589895016231600158').send(member.user.username + ", bienvenue sur la Compagnie !");
+  member.roles.add(member.guild.roles.cache.find(r => r.name === "Membre"));
   console.log("nouveau membre");
 });
 
 client.on('guildMemberRemove', member => {
-    member.guild.channels.cache.get('589895016231600158').send(guildMember.user.username + ', a quitté la Compagnie :(');
+    member.guild.channels.cache.get('589895016231600158').send(member.user.username + ', a quitté la Compagnie :(');
 });
 
 client.on('ready', () => {
