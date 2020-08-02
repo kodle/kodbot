3af8b1e8e12f8fc9bbc93eebf6d40746b05d5e2e
kodle <theobourdier1@gmail.com>
2020-08-02 09:30:48 +0200

    fix

--- server.js
+++ server.js
@@ -16,22 +16,13 @@ const token = process.env.TOKEN;
 // var giphy = require('giphy-api')(process.env.GIPHY);
 
 client.on('guildMemberAdd', member => {
-<<<<<<< HEAD
-  member.guild.channels.cache.get('556815696399564802').send(`🥳 <@${member.user.id}>, bienvenue sur la Compagnie !`);
-  member.roles.add(member.guild.roles.cache.find(r => r.name === "Gens Sympas"));
-=======
   member.guild.channels.cache.get('556815696399564802').send(`🥳 <@${member.user.id}>, bienvenue sur la Citronnerie !`);
   member.roles.add(member.guild.roles.cache.find(r => r.name === "Membre"));
->>>>>>> a45a42c04173fc5eabbdaee56fe5fab816c8717b
   console.log("nouveau membre");
 });
 
 client.on('guildMemberRemove', member => {
-<<<<<<< HEAD
-    member.guild.channels.cache.get('556815696399564802').send(`😢 <@${member.user.id}>, a quitté la Compagnie :(`);
-=======
     member.guild.channels.cache.get('556815696399564802').send(`😢 <@${member.user.id}>, a quitté la Citronnerie :(`);
->>>>>>> a45a42c04173fc5eabbdaee56fe5fab816c8717b
 }); // id lunarly 589895016231600158 / id general 556815696399564802
 
 client.on('ready', () => {
